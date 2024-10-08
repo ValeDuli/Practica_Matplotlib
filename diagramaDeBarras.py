95d00fe746117b65addf6611bcde4e92c69f0110
ValeDuli <valery_dur11@hotmail.com>
2024-10-07 18:52:53 -0600

    Prueba de Commit

---
+++ diagramaDeBarras.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np                          # Importacion de librerias para poder dibujar graficos
+                                            # Crear un alias, para evitar teclear los nombres completos
+                                            # de las librerias
+x=np.array(["Comedia", "Acción", "Terror"])
+y=np.array([3,1,10])
+colores=["blue","red","mediumaquamarine"]   # Cargar datos donde un eje (x) es  para los argumentos
+                                            # y el otro eje (y) es para los valores.
+                                            # Es opcional asignar color a cada una de las barras,
+                                            # si se desea hacerlo, se debe de escribir el color en inglés
+plt.bar(x,y,color=colores)
+plt.show()
